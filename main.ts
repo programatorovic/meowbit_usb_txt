001c3827b8dfccc6b2af1d15d1bcac140d5f3050
programatorovic <programatorovic@gmail.com>
2025-01-06 16:27:41 +0100

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,22 +1,17 @@
 //% color=#0fbc11 icon="\uf287" block="USB Serial"
 namespace USBSerial {
-    let usb: USB_VCP = new USB_VCP();
-
     //% block="initialize USB serial at baud rate %baud"
     export function initialize(baud: number): void {
-        usb.setBaudRate(baud);
+        serial.redirect(SerialPin.P0, SerialPin.P1, baud);
     }
 
     //% block="send string %text over USB"
     export function sendString(text: string): void {
-        usb.write(text + "\n");
+        serial.writeString(text + "\n");
     }
 
     //% block="read string from USB"
     export function readString(): string {
-        if (usb.any()) {
-            return usb.read();
-        }
-        return "";
+        return serial.readString();
     }
 }
